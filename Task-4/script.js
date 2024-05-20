7975c0daa381216d3b555fa40c90c23231f392b5
PinkiKumari16 <pinkikmr2672002@gmail.com>
2024-05-20 10:46:09 +0530

    add bg imae

--- Task-4/script.js
+++ Task-4/script.js
@@ -3,7 +3,7 @@ body.style.display = 'flex';
 body.style.justifyContent = 'center';
 
 let div = document.createElement('div');
-div.style.backgroundColor = 'pink';
+div.style.backgroundImage = 'url("https://img.freepik.com/free-photo/people-taking-photos-food_23-2149303525.jpg?w=1380&t=st=1705607469~exp=1705608069~hmac=278471867b312bc0088e9ffac7a2f27addf229e5e57bacefa17334eee38448ad")';
 div.style.height = '100vh';
 div.style.width = '30%'
 body.appendChild(div)
