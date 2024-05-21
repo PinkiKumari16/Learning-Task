be1d6f4245e883bc14a9fb3535661eb796b15c22
PinkiKumari16 <pinkikmr2672002@gmail.com>
2024-05-21 09:55:10 +0530

    set image

--- Task-6/script.js
+++ Task-6/script.js
@@ -1,4 +1,18 @@
 let happy = true;
+let UnHappyUrl = "https://emojiisland.com/cdn/shop/products/Unhappy_Face_Emoji_Icon_ios10_large.png?v=1571606093";
+let happyUrl = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/65532/happy-emoji-clipart-md.png";
+
+
+let div = document.createElement('div');
+div.style.height = '5em';
+div.style.width = '20vw';
+div.style.backgroundImage = `url(${happyUrl})`;
+div.style.backgroundSize = 'contain';
+div.style.margin = '10px';
+div.style.backgroundRepeat = 'no-repeat';
+
+document.body.prepend(div);
+
 let btn = document.getElementById('changeBtn');
 btn.style.padding = '0.5em';
 btn.style.fontSize = '1em'
@@ -6,19 +20,15 @@ btn.style.fontWeight = 'bold';
 btn.style.backgroundColor = "#0000ff";
 btn.style.color = '#ffffff';
 
-let UnHappyUrl = "https://emojiisland.com/cdn/shop/products/Unhappy_Face_Emoji_Icon_ios10_large.png?v=1571606093";
-let happyUrl = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/65532/happy-emoji-clipart-md.png";
 
 let emogiChange = () =>{
     happy = !happy;
-    let emoji = document.getElementById('happy');
-
     if (!happy){
-        emoji.innerText = "Un-Happy";
+        div.style.backgroundImage = `url(${UnHappyUrl})`;
         btn.innerText = 'Happy';
     }
     else{
-        emoji.innerHTML = 'Happy';
+        div.style.backgroundImage = `url(${happyUrl})`;
         btn.innerText = 'Un-Happy';
     }
 }
