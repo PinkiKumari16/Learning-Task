7381809be87a21a10621bb138081d0981fd4f9d5
PinkiKumari16 <pinkikmr2672002@gmail.com>
2024-05-22 18:02:18 +0530

    task-6 compated

--- Task-7/script.js
+++ Task-7/script.js
@@ -1,6 +1,6 @@
 let switchoff = () =>{
-    document.querySelector('#bulbImage').src = 'https://i.ibb.co/BGT8nNM/bulb-go-off-img.png';
-    document.querySelector('#catImage').src = 'https://i.ibb.co/tXR1RNY/cat-eyes-img.png';
+    document.querySelector('#bulbImage').src = './bulb-off.webp';
+    document.querySelector('#catImage').src = './cat-eyes.webp';
     document.querySelector('.on-switch').style.backgroundColor = '#22c55e';
     document.querySelector('.off-switch').style.backgroundColor = '#cbd2d9';
     document.querySelector('.switch-status').innerText = 'Switched off';
@@ -8,10 +8,10 @@ let switchoff = () =>{
 }
 
 let switchon = () =>{
-    document.querySelector('#bulbImage').src = 'https://i.ibb.co/BwHDN9W/bulb-go-on-img.png';
-    document.querySelector('#catImage').src = 'https://i.ibb.co/4MWrYQV/cat-img.png';
-    document.querySelector('.on-switch').style.backgroundColor = '#e12d39';
-    document.querySelector('.off-switch').style.backgroundColor = '#cbd2d9';
+    document.querySelector('#bulbImage').src = './bulb-on.webp';
+    document.querySelector('#catImage').src = './cat.webp';
+    document.querySelector('.on-switch').style.backgroundColor = '#cbd2d9';
+    document.querySelector('.off-switch').style.backgroundColor = '#e12d39';
     document.querySelector('.switch-status').innerText = 'Switched On';
 
 }
