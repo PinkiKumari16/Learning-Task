5e227152b2e53d8c7257cb9cf273696a5a24224b
pinkikumari16 <pinkikmr2672002@gmail.com>
2024-05-29 09:19:18 +0530

    our viewers

---
+++ Our Reviews/script.js
@@ -0,0 +1,67 @@
+// import Data from './data.json';
+// console.log(Data)
+
+const data = [
+    {
+        "image" : "./pinki.jpeg" ,
+        "name" : "Pinki",
+        "professtion" : "FRONT END DEVELOPER",
+        "content" : "I'am baby meggings health goth +1, Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humlebrag pickled coloring book salvia hoodle, cold-pressed four doliar toast everyday carry."
+    },
+    {
+        "image" : "./pinki.jpeg" ,
+        "name" : "Rinki",
+        "professtion" : "FULL STACK DEVELOPER",
+        "content" : "I'am baby meggings health goth +1, Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humlebrag pickled coloring book salvia hoodle, cold-pressed four doliar toast everyday carry."
+    },
+    {
+        "image" : "./pinki.jpeg" ,
+        "name" : "Kiran",
+        "professtion" : "FRONT END DEVELOPER",
+        "content" : "I'am baby meggings health goth +1, Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humlebrag pickled coloring book salvia hoodle, cold-pressed four doliar toast everyday carry."
+    }
+];
+
+let index = 0;
+let image = document.querySelector('img');
+let userName = document.querySelector('.name');
+let profession = document.querySelector('.profession');
+let content = document.querySelector('.contant');
+
+let randomInd = () =>{
+    return Math.floor(Math.random()*data.length);
+}
+
+let setData = (ind) => {
+    image.src = data[ind].image;
+    userName.textContent = data[ind].name;
+    profession.textContent = data[ind].professtion;
+    content.textContent = data[ind].content;
+}
+
+let button = document.querySelector('.btn');
+button.addEventListener('click', ()=>{
+    index = randomInd();
+    setData(index)
+
+})
+
+let arros = document.querySelectorAll('.arro');
+arros.forEach(arro => {
+    arro.addEventListener('click',()=>{
+        if(arro.id === 'back-arro'){
+            if(index === 0){
+                index=data.length;
+            }
+            index--;
+        }
+        else{
+            if(index === arro.length-1){
+                index = -1;
+            }
+            index++;
+            console.log(index)
+        }
+        setData(index)
+    })
+});
